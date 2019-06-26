855fedfdd8b32f1d07d715dd8a518d0c974f15d0
1131052033778 <1131052033778@nsl.ad.nec.co.jp>
2019-06-26 17:39:55 +0900

    fix unnecessary parenthesis

--- abc086/a.fsx
+++ abc086/a.fsx
@@ -2,5 +2,5 @@ module Main
 
 stdin.ReadLine().Split(' ') 
     |> Array.map int
-    |> fun(integers)  -> if (integers.[0] % 2) = 0 || (integers.[1] % 2) = 0 then "Even" else "Odd"
+    |> fun integers -> if (integers.[0] % 2) = 0 || (integers.[1] % 2) = 0 then "Even" else "Odd"
     |> printfn "%s"

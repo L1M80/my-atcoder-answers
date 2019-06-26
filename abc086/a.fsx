d0e0c52374b0c9acf176aa10e0a78a4d183e5f83
1131052033778 <1131052033778@nsl.ad.nec.co.jp>
2019-06-26 17:32:26 +0900

    solved abc086 a

---
+++ abc086/a.fsx
@@ -0,0 +1,6 @@
+module Main
+
+stdin.ReadLine().Split(' ') 
+    |> Array.map int
+    |> fun(integers)  -> if (integers.[0] % 2) = 0 || (integers.[1] % 2) = 0 then "Even" else "Odd"
+    |> printfn "%s"

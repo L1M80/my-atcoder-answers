2ea0c9eb807ed0b961fabcf10e46dad113fabf40
1131052033778 <1131052033778@nsl.ad.nec.co.jp>
2019-06-26 18:01:36 +0900

    format

--- abc086/a.fsx
+++ abc086/a.fsx
@@ -1,6 +1,8 @@
 module Main
 
-stdin.ReadLine().Split(' ') 
-    |> Array.map int
-    |> fun integers -> if (integers.[0] % 2) = 0 || (integers.[1] % 2) = 0 then "Even" else "Odd"
-    |> printfn "%s"
+stdin.ReadLine().Split(' ')
+|> Array.map int
+|> fun integers ->
+    if (integers.[0] % 2) = 0 || (integers.[1] % 2) = 0 then "Even"
+    else "Odd"
+|> printfn "%s"

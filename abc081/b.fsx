af0dd946f4fc185aec677435e3e44bd3bc74e7b4
1131052033778 <1131052033778@nsl.ad.nec.co.jp>
2019-06-27 11:16:12 +0900

    solved abc081 b

---
+++ abc081/b.fsx
@@ -0,0 +1,17 @@
+// https://atcoder.jp/contests/abc081/tasks/abc081_b
+
+module Main
+
+let _ = stdin.ReadLine() |> int
+
+let rec isEven =
+    fun i ->
+        match i with
+        | i when i % 2L = 0L -> 1L + isEven (i / 2L)
+        | _ -> 0L
+
+stdin.ReadLine().Split(' ')
+|> Array.map int64
+|> Array.map isEven
+|> Array.min
+|> stdout.WriteLine
